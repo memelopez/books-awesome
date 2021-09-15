93c1c5ba01c4184cdaeca5385c55bf5fc537af0d
ixboy <ismaelixboy@gmail.com>
2021-09-15 08:26:31 +0100

    UI class

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// JS for booklist
-// Book Class: Represents a Book
 class Book {
   constructor(title, author) {
     this.title = title;
@@ -7,7 +5,6 @@ class Book {
   }
 }
 
-// Store Class: Handles Storage
 class Store {
   static getBooks() {
     let books;
@@ -35,55 +32,59 @@ class Store {
   }
 }
 
-// UI Class: Handle UI Tasks
-function addBookToList(book) {
-  const list = document.querySelector('#book-list');
-
-  const item = document.createElement('li');
-  item.className = 'd-flex flex-column border-bottom border-2 py-2';
-
-  const pBook1 = document.createElement('p');
-  pBook1.textContent = book.title;
-  pBook1.className = 'py-1 m-0';
-
-  const pBook2 = document.createElement('p');
-  pBook2.textContent = book.author;
-  pBook2.className = 'py-1 m-0';
-
-  const pRmv = document.createElement('button');
-  pRmv.textContent = 'Remove';
-  pRmv.className = 'py-1 m-0 rmv';
-
-  item.appendChild(pBook1);
-  item.appendChild(pBook2);
-  item.appendChild(pRmv);
-
-  list.appendChild(item);
-}
-
-function displayBooks() {
-  // Gets booklist from local storage
-  const books = Store.getBooks();
-  books.forEach((book) => addBookToList(book));
-}
 
 function clearOut() {
   document.querySelector('#book-title').value = '';
   document.querySelector('#book-author').value = '';
 }
 
-function showAlert(message, className) {
-  const div = document.createElement('div');
-  div.className = `alert alert-${className} p-1 mb-1`;
-  div.appendChild(document.createTextNode(message));
-  const container = document.querySelector('.box');
-  const formDiv = document.querySelector('#div4form');
-  container.insertBefore(div, formDiv);
-  setTimeout(() => document.querySelector('.alert').remove(), 2000);
+// UI Class: Handle UI Tasks
+class UI {
+  static addBookToList(book) {
+    const list = document.querySelector('#book-list');
+
+    const item = document.createElement('li');
+    item.className = 'd-flex flex-column border-bottom border-2 py-2';
+
+    const pBook1 = document.createElement('p');
+    pBook1.textContent = book.title;
+    pBook1.className = 'py-1 m-0';
+
+    const pBook2 = document.createElement('p');
+    pBook2.textContent = book.author;
+    pBook2.className = 'py-1 m-0';
+
+    const pRmv = document.createElement('button');
+    pRmv.textContent = 'Remove';
+    pRmv.className = 'py-1 m-0 rmv';
+
+    item.appendChild(pBook1);
+    item.appendChild(pBook2);
+    item.appendChild(pRmv);
+
+    list.appendChild(item);
+  }
+
+  static displayBooks() {
+    // Gets booklist from local storage
+    const books = Store.getBooks();
+    books.forEach((book) => this.addBookToList(book));
+  }
+
+  static showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert alert-${className} p-1 mb-1`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.box');
+    const formDiv = document.querySelector('#div4form');
+    container.insertBefore(div, formDiv);
+    setTimeout(() => document.querySelector('.alert').remove(), 2000);
+  }
 }
 
+
 // Event: Display Books
-document.addEventListener('DOMContentLoaded', displayBooks());
+document.addEventListener('DOMContentLoaded', UI.displayBooks());
 
 // Event: Add book
 document.querySelector('#book-form').addEventListener('submit', (e) => {
@@ -96,14 +97,15 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 
   // Validate
   if (titleI === '' || authorI === '') {
-    showAlert('title and author must not be empty', 'success');
+    UI.showAlert('title and author must not be empty', 'success');
   } else {
-    const books = Store.getBooks();
-    const book = new Book(titleI, authorI);
-    books.push(book);
+    const books = Store.getBooks(); // get books from local storage
+    const book = new Book(titleI, authorI); // new instance of Book
 
-    Store.setBooks(books);
-    clearOut();
+    books.push(book); // push new book into books array
+
+    Store.setBooks(books); // sets new books array in local storage
+
     // Reload page
     // eslint-disable-next-line no-restricted-globals
     location.reload();
