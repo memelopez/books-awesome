951dc30a9b4b7ec71efd111705db49a252236bf7
memelopez <meme30lopez@gmail.com>
2022-01-10 10:12:53 -0600

    Add separate files for new modules

---
+++ modules/store.js
@@ -0,0 +1,26 @@
+export default class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  }
+
+  static setBooks(books) {
+    if (books.length > 0) {
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+  }
+
+  static removeBook(index) {
+    const books = this.getBooks();
+
+    books.splice(index, 1);
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
